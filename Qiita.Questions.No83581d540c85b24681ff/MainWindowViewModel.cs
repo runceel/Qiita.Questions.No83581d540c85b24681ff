8b6402690891a34b50f74ef444930468b5061469
Kazuki Ota <kaota@microsoft.com>
2021-07-28 15:31:41 +0900

    Add project files.

---
+++ Qiita.Questions.No83581d540c85b24681ff/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Qiita.Questions.No83581d540c85b24681ff
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public ReadOnlyReactivePropertySlim<string> Message { get; }
+        public ReactivePropertySlim<Setting> EditTarget { get; } = new ReactivePropertySlim<Setting>();
+        public ReactiveCommand OkCommand { get; }
+
+        public ReactiveCommand ChangeEditTargetValueCommand { get; }
+        public MainWindowViewModel()
+        {
+            EditTarget.Value = new Setting();
+
+            OkCommand = EditTarget
+                .Select(x => x.Width.CombineLatest(x.Height, (a, b) => a > 0 && b > 0))
+                .Switch()
+                .ToReactiveCommand();
+
+            // Ok ボタンが押せたら更新されるプロパティを定義しているだけです（動作確認用）
+            Message = OkCommand.Select(_ => DateTime.Now.ToString()).ToReadOnlyReactivePropertySlim();
+
+            // EditTarget の値を更新するコマンド
+            ChangeEditTargetValueCommand = new ReactiveCommand()
+                .WithSubscribe(() => EditTarget.Value = new Setting());
+        }
+    }
+}
---
+++ Qiita.Questions.No83581d540c85b24681ff/Setting.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qiita.Questions.No83581d540c85b24681ff
+{
+    public class Setting : BindableBase
+    {
+        public ReactivePropertySlim<int> Width { get; } = new ReactivePropertySlim<int>();
+        public ReactiveProperty<int> Height { get; } = new ReactiveProperty<int>();
+    }
+}
